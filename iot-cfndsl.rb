d56aad92e8611c3d47d7f5337e52c8bd7ee554a3
Tim Mattison <timmatt@amazon.com>
2016-09-07 12:12:31 +0900

    TEMP

--- iot-cfndsl.rb
+++ iot-cfndsl.rb
@@ -1,27 +1,14 @@
 #!/usr/bin/env ruby
 
-CloudFormation {
+CloudFormation do
   Description "Test"
-
-  Parameter("One") {
-    String
-    Default "Test"
-    MaxLength 15
-  }
-
-  Output(:One, FnBase64(Ref("One")))
 
   IoT_TopicRule(:MyTopicRule) {
     RuleName "MyTestRule"
     TopicRulePayload {
-      Actions [
-                  Republish [{
-                    RoleArn "asdf"
-                  }]
-              ]
       Description "my description"
       RuleDisabled false
       Sql "SELECT * FROM 'thingtopic'"
     }
   }
-}
+end
